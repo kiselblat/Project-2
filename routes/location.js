3f3d043b701de013a995105a526a4e221476fe45
Roisin Hayes Owens <roisinhayesowens@gmail.com>
2019-07-25 20:10:18 -0500

    location

--- routes/location.js
+++ routes/location.js
@@ -1,41 +1,42 @@
 var db = require("../models");
 
-module.exports = function(app) {
-    app.get("/api/locations", function(req, res) {
-        db.Location.findAll({}).then(function (dbLocation) {
-            res.json(dbLocation);
+module.exports = function (app) {
+  app.get("/api/locations", function (req, res) {
+    db.Location.findAll({
+    }).then(function (dbLocations) {
+      res.json(dbLocations);
     });
   });
 
 
 
-app.get("/api/locations/:id", function(req, res) {
+  app.get("/api/locations/:id", function (req, res) {
     // Find one Author with the id in req.params.id and return them to the user with res.json
     db.Location.findOne({
       where: {
         id: req.params.id
-      }
-    }).then(function(dbLocation) {
-      res.json(dbLocation);
+      }, include: [db.Category]
+    }).then(function(dbLocations) {
+      res.json(dbLocations);
     });
   });
 
-  app.post("/api/locations", function(req, res) {
+  app.post("/api/locations", function (req, res) {
     // Create an Author with the data available to us in req.body
     console.log(req.body);
-    db.Location.create(req.body).then(function(dbLocation) {
-      res.json(dbLocation);
+    db.Location.create(req.body).then(function (dbLocations) {
+      res.json(dbLocations);
     });
   });
 
-  app.delete("/api/locations/:id", function(req, res) {
+  app.delete("/api/locations/:id", function (req, res) {
     // Delete the Author with the id available to us in req.params.id
     db.Location.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function(dbLocation) {
-      res.json(dbLocation);
+    }).then(function (dbLocations) {
+      res.json(dbLocations);
     });
   });
 
