6d9e240462d7b6418c828f4344d726dd155bbc69
Tom <tsechrist@outlook.com>
2019-07-31 15:41:33 -0500

    Dumps contents of items into txt with json.stringify

--- public/js/mailer.js
+++ public/js/mailer.js
@@ -15,27 +15,34 @@ var postMail = function(email) {
 
 encloseMail = function () {
   console.log("enclosing mail...");
-  var $messageBody = $("<div/>");
-  var $additionalMessage = $("<p/>").text($messageText.val().trim());
-  // TODO: Make table of items here
-  var $inventoryTable = $("<p/>").text("This is where the inventory could go.");
+  $.ajax({
+    url: "api/inventory",
+    type: "GET"
+  }).then(function(dbItems) {
 
-  $messageBody.append($additionalMessage, $("<br>"), $inventoryTable);
-
-  var newMail = {
-    // from: "Email Sender" // this gets assigned in the route to avoid hardcoding
-    to: $emailInput.val().trim(),
-    subject: "Item Report from Nesterly",
-    html: $messageBody.prop("outerHTML"),
-    attachments: [
-      {
-        filename: "attachment.txt",
-        content: "This is an attachment."
-      }
-    ]
-  };
-  postMail(newMail);
-  resetForm();
+    var $messageBody = $("<div/>");
+    var $additionalMessage = $("<p/>").text($messageText.val().trim());
+    // TODO: Make table of items here
+    var $inventoryTable = $("<p/>").text("This is where the inventory could go.");
+    
+    $messageBody.append($additionalMessage, $("<br>"), $inventoryTable);
+    
+    var newMail = {
+      // from: "Email Sender" // this gets assigned in the route to avoid hardcoding
+      to: $emailInput.val().trim(),
+      subject: "Item Report from Nesterly",
+      html: $messageBody.prop("outerHTML"),
+      attachments: [
+        {
+          filename: "attachment.txt",
+          content: JSON.stringify(dbItems)
+        }
+      ]
+    };
+    postMail(newMail);
+    console.log("...and sent!");
+    resetForm();
+  });
 };
 
 var resetForm = function () {
